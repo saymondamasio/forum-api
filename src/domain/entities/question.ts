203d69a0f294c7a71bdbf2c79ef9647908f28be0
Saymon Damásio <saymon.damasio95@gmail.com>
2023-09-02 10:14:09 -0300

    feat: create value object slug

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,7 +1,9 @@
 import { randomUUID } from 'node:crypto'
+import { Slug } from './value-objects/slug'
 
 interface QuestionProps {
   id: string
+  slug?: Slug
   title: string
   content: string
   authorId: string
@@ -9,12 +11,14 @@ interface QuestionProps {
 
 export class Question {
   public id: string
+  public slug: Slug
   public title: string
   public content: string
   public authorId: string
 
   constructor(props: QuestionProps, id?: string) {
     this.title = props.title
+    this.slug = Slug.createFromText(props.title)
     this.content = props.content
     this.authorId = props.authorId
     this.id = id ?? randomUUID()
