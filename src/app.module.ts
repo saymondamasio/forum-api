f61151dbbaeb6d72c9c73856a5510464ed7e3b1d
Saymon Damásio <saymon.damasio95@gmail.com>
2023-09-27 19:25:48 -0300

    chore: configure nestjs

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common'
+import { CreateAccountController } from './controllers/create-account.controller'
+import { PrismaService } from './prisma/prisma.service'
+
+@Module({
+  imports: [],
+  controllers: [CreateAccountController],
+  providers: [PrismaService],
+})
+export class AppModule {}
--- src/core/events/domain-event.ts
+++
@@ -1,6 +0,0 @@
-import { UniqueEntityID } from '../entities/unique-entity-id'
-
-export interface DomainEvent {
-  ocurredAt: Date
-  getAggregateId(): UniqueEntityID
-}
