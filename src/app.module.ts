e962bed9d95cb860b05f0f2ecc2caa9b96cf74f1
Saymon Damásio <saymon.damasio95@gmail.com>
2023-09-28 10:21:58 -0300

    feat: environments variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { PrismaService } from './prisma/prisma.service'
 
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env'
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
