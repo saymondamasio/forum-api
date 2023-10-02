af9134fdc4a05254de7f4e2199e7d8748b71885d
Saymon Damásio <saymon.damasio95@gmail.com>
2023-10-02 14:47:34 -0300

    feat: authenticate

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { PrismaService } from './prisma/prisma.service'
 
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
+import { AuthenticateController } from './controllers/authenticate.controller'
 
 @Module({
   imports: [
@@ -11,8 +13,9 @@ import { envSchema } from './env'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
-  controllers: [CreateAccountController],
+  controllers: [CreateAccountController, AuthenticateController],
   providers: [PrismaService],
 })
 export class AppModule {}
