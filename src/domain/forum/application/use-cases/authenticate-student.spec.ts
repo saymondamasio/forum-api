a7d2223736bf3dec71eed73f78a391053b8fa58a
Saymon Damásio <saymon.damasio95@gmail.com>
2023-11-15 15:32:52 -0300

    test: authenticate and register student

---
+++ src/domain/forum/application/use-cases/authenticate-student.spec.ts
@@ -0,0 +1,44 @@
+import { AuthenticateStudentUseCase } from './authenticate-student'
+
+import InMemoryStudentsRepository from 'test/repositories/in-memory-students-repository'
+import { FakeHasher } from 'test/crptography/fake-hasher'
+import { FakeEncrypter } from 'test/crptography/fake-encrypter'
+import { makeStudent } from 'test/factories/make-students'
+
+let inMemoryStudentsRepository: InMemoryStudentsRepository
+let fakeHasher: FakeHasher
+let fakeEncrypter: FakeEncrypter
+let authenticateStudent: AuthenticateStudentUseCase
+
+describe('Authenticate Student', () => {
+  beforeEach(() => {
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+    fakeHasher = new FakeHasher()
+    fakeEncrypter = new FakeEncrypter()
+
+    authenticateStudent = new AuthenticateStudentUseCase(
+      inMemoryStudentsRepository,
+      fakeHasher,
+      fakeEncrypter,
+    )
+  })
+
+  it('should be able to authenticate  a student', async () => {
+    const student = makeStudent({
+      email: 'john@example.com',
+      password: await fakeHasher.hash('123456'),
+    })
+
+    inMemoryStudentsRepository.items.push(student)
+
+    const result = await authenticateStudent.execute({
+      email: 'john@example.com',
+      password: '123456',
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      accessToken: expect.any(String),
+    })
+  })
+})
--- src/domain/forum/application/use-cases/authenticate-student.ts
+++ src/domain/forum/application/use-cases/authenticate-student.ts
@@ -1,13 +1,10 @@
 import { Injectable } from '@nestjs/common'
 
 import { Either, left, right } from '@/core/either'
-import { Student } from '../../enterprise/entities/student'
 import { StudentsRepository } from '../repositories/students-repository'
-import { HashGenerator } from '../cryptography/hash-generator'
 import { HashComparer } from '../cryptography/hash-comparer'
 import { Encrypter } from '../cryptography/encrypter'
 
-import { StudentsAlreadyExistsError } from './erros/students-already-exits-error'
 import { WrongCredentialsError } from './erros/wrong-credentials-error'
 
 interface AuthenticateStudentUseCaseRequest {
@@ -23,7 +20,7 @@ type RegisterStudentUseCaseResponse = Either<
 >
 
 @Injectable()
-export class RegisterStudentUseCase {
+export class AuthenticateStudentUseCase {
   constructor(
     private studentsRepository: StudentsRepository,
     private hashCompare: HashComparer,
