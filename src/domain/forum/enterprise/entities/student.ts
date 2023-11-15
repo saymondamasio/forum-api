b4d86b0c551a83908a9e95e13f6d328e2d576cb9
Saymon Damásio <saymon.damasio95@gmail.com>
2023-11-15 10:45:52 -0300

    feat: create criptography gateway

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -3,11 +3,21 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 interface StudentProps {
   name: string
+  email: string
+  password: string
 }
 
 export class Student extends Entity<StudentProps> {
   get name() {
     return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get password() {
+    return this.props.password
   }
 
   static create(props: Student, id?: UniqueEntityID) {
