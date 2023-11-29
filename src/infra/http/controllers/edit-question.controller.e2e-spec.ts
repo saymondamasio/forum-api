dd2aa1ad3172dc7255ad8bcd1d54cf3f816d23bd
Saymon Damásio <saymon.damasio95@gmail.com>
2023-11-28 22:08:28 -0300

    feat: receive attachments in edit question

--- src/infra/http/controllers/edit-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/edit-question.controller.e2e-spec.ts
@@ -9,18 +9,27 @@ import { DatabaseModule } from '@/infra/database/database.module'
 
 import { StudentFactory } from 'test/factories/make-student'
 import { QuestionFactory } from 'test/factories/make-question'
+import { AttachmentFactory } from 'test/factories/make-attachment'
+import { QuestionAttachmentFactory } from 'test/factories/make-question-attachments'
 
 describe('Edit question (E2E)', () => {
   let app: INestApplication
   let jwtService: JwtService
   let questionFactory: QuestionFactory
   let studentFactory: StudentFactory
+  let attachmentFactory: AttachmentFactory
+  let questionAttachmentFactory: QuestionAttachmentFactory
   let prisma: PrismaService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory, QuestionFactory],
+      providers: [
+        StudentFactory,
+        QuestionFactory,
+        QuestionAttachmentFactory,
+        AttachmentFactory,
+      ],
     }).compile()
 
     app = moduleRef.createNestApplication()
@@ -28,6 +37,8 @@ describe('Edit question (E2E)', () => {
     jwtService = moduleRef.get(JwtService)
     studentFactory = moduleRef.get(StudentFactory)
     questionFactory = moduleRef.get(QuestionFactory)
+    questionAttachmentFactory = moduleRef.get(QuestionAttachmentFactory)
+    attachmentFactory = moduleRef.get(AttachmentFactory)
     prisma = moduleRef.get(PrismaService)
 
     await app.init()
@@ -36,13 +47,28 @@ describe('Edit question (E2E)', () => {
   test('[PUT] /questions', async () => {
     const user = await studentFactory.makePrismaStudent()
 
+    const accessToken = jwtService.sign({
+      sub: user.id.toString(),
+    })
+
     const question = await questionFactory.makePrismaQuestion({
       authorId: user.id,
     })
 
-    const accessToken = jwtService.sign({
-      sub: user.id.toString(),
+    const attachment1 = await attachmentFactory.makePrismaAttachment()
+    const attachment2 = await attachmentFactory.makePrismaAttachment()
+
+    await questionAttachmentFactory.makePrismaAttachment({
+      attachmentId: attachment1.id,
+      questionId: question.id,
     })
+
+    await questionAttachmentFactory.makePrismaAttachment({
+      attachmentId: attachment2.id,
+      questionId: question.id,
+    })
+
+    const attachment3 = await attachmentFactory.makePrismaAttachment()
 
     const response = await request(app.getHttpServer())
       .put(`/questions/${question.id.toString()}`)
@@ -50,6 +76,7 @@ describe('Edit question (E2E)', () => {
       .send({
         title: 'New Question',
         content: 'New content',
+        attachments: [attachment1.id.toString(), attachment3.id.toString()],
       })
 
     expect(response.statusCode).toBe(204)
@@ -62,5 +89,23 @@ describe('Edit question (E2E)', () => {
     })
 
     expect(questionOnDatabase).toBeTruthy()
+
+    const attachmentOnDatabase = await prisma.attachment.findMany({
+      where: {
+        questionId: questionOnDatabase?.id,
+      },
+    })
+
+    expect(attachmentOnDatabase).toHaveLength(2)
+    expect(attachmentOnDatabase).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: attachment1.id.toString(),
+        }),
+        expect.objectContaining({
+          id: attachment3.id.toString(),
+        }),
+      ]),
+    )
   })
 })
--- src/infra/http/controllers/edit-question.controller.ts
+++ src/infra/http/controllers/edit-question.controller.ts
@@ -16,6 +16,7 @@ import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-q
 const editQuestionBodySchema = z.object({
   title: z.string(),
   content: z.string(),
+  attachments: z.array(z.string().uuid()),
 })
 
 type EditQuestionBodySchema = z.infer<typeof editQuestionBodySchema>
@@ -34,13 +35,13 @@ export class EditQuestionController {
     body: EditQuestionBodySchema,
     @Param('id') questionId: string,
   ) {
-    const { content, title } = body
+    const { content, title, attachments } = body
     const userId = user.sub
 
     const result = await this.editQuestion.execute({
       authorId: userId,
       title,
-      attachmentIds: [],
+      attachmentIds: attachments,
       content,
       questionId,
     })
