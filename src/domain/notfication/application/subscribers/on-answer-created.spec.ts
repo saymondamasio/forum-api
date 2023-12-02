ad2ace2c204733811bc81566cee0d249329e3b09
Saymon Damásio <saymon.damasio95@gmail.com>
2023-12-02 17:53:03 -0300

    fix: errors ts

--- src/domain/notfication/application/subscribers/on-answer-created.spec.ts
+++ src/domain/notfication/application/subscribers/on-answer-created.spec.ts
@@ -1,23 +1,30 @@
-import { makeAnswer } from 'test/factories/make-answer'
-import { OnAnswerCreated } from './on-answer-created'
-import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
-import InMemoryAnswerAttachmentsRepository from 'test/repositories/in-memory-answer-attachments-repository'
-import InMemoryQuestionsRepository from 'test/repositories/in-memory-questions-repository'
-import InMemoryQuestionAttachmentRepository from 'test/repositories/in-memory-question-attachments-repository'
+import { SpyInstance } from 'vitest'
+
 import {
   SendNotificationUseCase,
   SendNotificationUseCaseRequest,
   SendNotificationUseCaseResponse,
 } from '../use-cases/send-notification'
+
+import { OnAnswerCreated } from './on-answer-created'
+
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import InMemoryAnswerAttachmentsRepository from 'test/repositories/in-memory-answer-attachments-repository'
+import InMemoryQuestionsRepository from 'test/repositories/in-memory-questions-repository'
+import InMemoryQuestionAttachmentRepository from 'test/repositories/in-memory-question-attachments-repository'
 import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository'
 import { makeQuestion } from 'test/factories/make-question'
-import { SpyInstance } from 'vitest'
 import { waitFor } from 'test/utils/wait-for'
+import InMemoryStudentsRepository from 'test/repositories/in-memory-students-repository'
+import InMemoryAttachmentsRepository from 'test/repositories/in-memory-attachments-repository'
 
 let inMemoryQuestionAttachmentRepository: InMemoryQuestionAttachmentRepository
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
 let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryStudentsRepository: InMemoryStudentsRepository
+let inMemoryAttachmentsRepository: InMemoryAttachmentsRepository
 let inMemoryNotificationsRepository: InMemoryNotificationsRepository
 
 let sendNotificationUseCase: SendNotificationUseCase
@@ -33,8 +40,12 @@ describe('On Answer Created', () => {
       new InMemoryQuestionAttachmentRepository()
     inMemoryAnswerAttachmentsRepository =
       new InMemoryAnswerAttachmentsRepository()
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+    inMemoryAttachmentsRepository = new InMemoryAttachmentsRepository()
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
       inMemoryQuestionAttachmentRepository,
+      inMemoryAttachmentsRepository,
+      inMemoryStudentsRepository,
     )
     inMemoryAnswersRepository = new InMemoryAnswersRepository(
       inMemoryAnswerAttachmentsRepository,
--- src/domain/notfication/application/subscribers/on-question-best-answer-chosen.spec.ts
+++ src/domain/notfication/application/subscribers/on-question-best-answer-chosen.spec.ts
@@ -1,23 +1,30 @@
-import { makeAnswer } from 'test/factories/make-answer'
-import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
-import InMemoryAnswerAttachmentsRepository from 'test/repositories/in-memory-answer-attachments-repository'
-import InMemoryQuestionsRepository from 'test/repositories/in-memory-questions-repository'
-import InMemoryQuestionAttachmentRepository from 'test/repositories/in-memory-question-attachments-repository'
+import { SpyInstance } from 'vitest'
+
 import {
   SendNotificationUseCase,
   SendNotificationUseCaseRequest,
   SendNotificationUseCaseResponse,
 } from '../use-cases/send-notification'
+
+import { OnQuestionAnswerChosenCreated } from './on-question-best-answer-chosen'
+
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import InMemoryAnswerAttachmentsRepository from 'test/repositories/in-memory-answer-attachments-repository'
+import InMemoryQuestionsRepository from 'test/repositories/in-memory-questions-repository'
+import InMemoryQuestionAttachmentRepository from 'test/repositories/in-memory-question-attachments-repository'
 import { InMemoryNotificationsRepository } from 'test/repositories/in-memory-notifications-repository'
 import { makeQuestion } from 'test/factories/make-question'
-import { SpyInstance } from 'vitest'
 import { waitFor } from 'test/utils/wait-for'
-import { OnQuestionAnswerChosenCreated } from './on-question-best-answer-chosen'
+import InMemoryStudentsRepository from 'test/repositories/in-memory-students-repository'
+import InMemoryAttachmentsRepository from 'test/repositories/in-memory-attachments-repository'
 
 let inMemoryQuestionAttachmentRepository: InMemoryQuestionAttachmentRepository
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
 let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryStudentsRepository: InMemoryStudentsRepository
+let inMemoryAttachmentsRepository: InMemoryAttachmentsRepository
 let inMemoryNotificationsRepository: InMemoryNotificationsRepository
 
 let sendNotificationUseCase: SendNotificationUseCase
@@ -33,8 +40,12 @@ describe('On Question Best Answer Chosen', () => {
       new InMemoryQuestionAttachmentRepository()
     inMemoryAnswerAttachmentsRepository =
       new InMemoryAnswerAttachmentsRepository()
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+    inMemoryAttachmentsRepository = new InMemoryAttachmentsRepository()
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository(
       inMemoryQuestionAttachmentRepository,
+      inMemoryAttachmentsRepository,
+      inMemoryStudentsRepository,
     )
     inMemoryAnswersRepository = new InMemoryAnswersRepository(
       inMemoryAnswerAttachmentsRepository,
