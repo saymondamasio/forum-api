f3cf4a9c7469cb59d805686d4111c07c97b0c168
Saymon Damásio <saymon.damasio95@gmail.com>
2023-12-02 15:09:44 -0300

    feat(domain): comment answer with author

--- src/domain/forum/application/use-cases/list-answer-comments.spec.ts
+++ src/domain/forum/application/use-cases/list-answer-comments.spec.ts
@@ -1,37 +1,52 @@
-import { ListAnswerCommentsUseCase } from './list-answer-comments'
-import { makeAnswerComment } from 'test/factories/make-answer-comment'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+import { ListAnswerCommentsUseCase } from './list-answer-comments'
+
+import { makeAnswerComment } from 'test/factories/make-answer-comment'
 import InMemoryAnswerCommentsRepository from 'test/repositories/in-memory-answer-comments-repository'
+import InMemoryStudentsRepository from 'test/repositories/in-memory-students-repository'
+import { makeStudent } from 'test/factories/make-student'
 
 let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository
+let inMemoryStudentsRepository: InMemoryStudentsRepository
 let listAnswerComments: ListAnswerCommentsUseCase
 
 describe('List Answer Comments', () => {
   beforeEach(() => {
-    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository()
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository(
+      inMemoryStudentsRepository,
+    )
     listAnswerComments = new ListAnswerCommentsUseCase(
       inMemoryAnswerCommentsRepository,
     )
   })
 
   it('should be able to list answer comments', async () => {
-    await inMemoryAnswerCommentsRepository.create(
-      makeAnswerComment({
-        answerId: new UniqueEntityID('answer-1'),
-      }),
-    )
+    const student = makeStudent({
+      name: 'John Doe',
+    })
 
-    await inMemoryAnswerCommentsRepository.create(
-      makeAnswerComment({
-        answerId: new UniqueEntityID('answer-1'),
-      }),
-    )
+    inMemoryStudentsRepository.items.push(student)
 
-    await inMemoryAnswerCommentsRepository.create(
-      makeAnswerComment({
-        answerId: new UniqueEntityID('answer-1'),
-      }),
-    )
+    const comment1 = makeAnswerComment({
+      answerId: new UniqueEntityID('answer-1'),
+      authorId: student.id,
+    })
+    const comment2 = makeAnswerComment({
+      answerId: new UniqueEntityID('answer-1'),
+      authorId: student.id,
+    })
+    const comment3 = makeAnswerComment({
+      answerId: new UniqueEntityID('answer-1'),
+      authorId: student.id,
+    })
+
+    await inMemoryAnswerCommentsRepository.create(comment1)
+
+    await inMemoryAnswerCommentsRepository.create(comment2)
+
+    await inMemoryAnswerCommentsRepository.create(comment3)
 
     const result = await listAnswerComments.execute({
       page: 1,
@@ -41,15 +56,38 @@ describe('List Answer Comments', () => {
     expect(result.isRight()).toBe(true)
 
     if (result.isRight()) {
-      expect(result.value.answerComments).toHaveLength(3)
+      expect(result.value.comments).toHaveLength(3)
+      expect(result.value.comments).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            author: 'John Doe',
+            commentId: comment1.id,
+          }),
+          expect.objectContaining({
+            author: 'John Doe',
+            commentId: comment2.id,
+          }),
+          expect.objectContaining({
+            author: 'John Doe',
+            commentId: comment3.id,
+          }),
+        ]),
+      )
     }
   })
 
   it('should be able to list paginated answer comments', async () => {
+    const student = makeStudent({
+      name: 'John Doe',
+    })
+
+    inMemoryStudentsRepository.items.push(student)
+
     Array.from({ length: 30 }).forEach(async () => {
       await inMemoryAnswerCommentsRepository.create(
         makeAnswerComment({
           answerId: new UniqueEntityID('answer-1'),
+          authorId: student.id,
         }),
       )
     })
@@ -62,7 +100,7 @@ describe('List Answer Comments', () => {
     expect(result.isRight()).toBe(true)
 
     if (result.isRight()) {
-      expect(result.value.answerComments).toHaveLength(10)
+      expect(result.value.comments).toHaveLength(10)
     }
   })
 })
--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,18 +1,52 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
+
+import InMemoryStudentsRepository from './in-memory-students-repository'
 
 const PAGE_SIZE = 20
 
 export default class InMemoryAnswerCommentsRepository
   implements AnswerCommentsRepository
 {
+  constructor(private studentsRepository: InMemoryStudentsRepository) {}
+
   async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
     const answerComments = this.items
       .filter((item) => item.answerId.toString() === answerId)
       .slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE)
 
     return answerComments
+  }
+
+  async findManyByAnswerIdWithAuthor(
+    answerId: string,
+    { page }: PaginationParams,
+  ) {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * PAGE_SIZE, page * PAGE_SIZE)
+
+    return answerComments.map((answerComments) => {
+      const author = this.studentsRepository.items.find((student) =>
+        student.id.equals(answerComments.authorId),
+      )
+
+      if (!author)
+        throw new Error(
+          `Author with ID ${answerComments.authorId.toString()} not found`,
+        )
+
+      return CommentWithAuthor.create({
+        commentId: answerComments.id,
+        content: answerComments.content,
+        authorId: answerComments.authorId,
+        author: author.name,
+        createdAt: answerComments.createdAt,
+        updatedAt: answerComments.updatedAt,
+      })
+    })
   }
 
   async delete(answerComment: AnswerComment): Promise<void> {
